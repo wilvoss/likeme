d1414663d57c28dcafcbae87b44cbe656c6c41e0
wilvoss <wilv@allovus.com>
2023-08-11 21:48:38 -0700

    likeme 3.9.67 - css work for small phones
    small animation bug fix

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -21,7 +21,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '3.9.65',
+    currentVersion: '3.9.67',
     deviceHasTouch: true,
     wallpaperNames: ['square', 'circle', 'triangle', 'hexagon'],
     currentWallpaper: '',
