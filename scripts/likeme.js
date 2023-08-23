34fe9c2ee8de5c53c1b05f3ca1a166d37d69730c
wilvoss <wilv@allovus.com>
2023-08-23 16:34:51 -0700

    likeme 4.1.16 - removed version history link

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.1.15',
+    currentVersion: '4.1.16',
     deviceHasTouch: true,
     wallpaperNames: ['square', 'circle', 'triangle', 'hexagon'],
     currentWallpaper: '',
