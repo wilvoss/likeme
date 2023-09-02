ebdd569195e9e5ac935cfcae2db87d6d76766421
wilvoss <wilv@allovus.com>
2023-09-02 12:22:05 -0700

    likeme 4.2.045 - fixed sharing text bug

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.040',
+    currentVersion: '4.2.045',
     deviceHasTouch: true,
     wallpaperNames: ['square', 'circle', 'triangle', 'hexagon'],
     currentWallpaper: '',
