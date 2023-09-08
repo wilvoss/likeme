c1427e2c77d17dc9f729ed7d7dcad95be42e5ba9
wilvoss <wilv@allovus.com>
2023-09-08 12:17:59 -0700

    likeme 4.2.092 - dark mode soft-light

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.091',
+    currentVersion: '4.2.092',
     deviceHasTouch: true,
     wallpaperNames: ['square', 'circle', 'triangle', 'hexagon'],
     currentWallpaper: '',
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = '4.2.091';
+const CACHE_VERSION = '4.2.092';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
