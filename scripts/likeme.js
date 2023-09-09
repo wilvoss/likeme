dd01b006bbfbde83bf3502b5cd5f0efe82f3900a
wilvoss <wilv@allovus.com>
2023-09-08 18:43:43 -0700

    likme 4.2.097 - added og: twitter plaque back

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.095',
+    currentVersion: '4.2.097',
     deviceHasTouch: true,
     wallpaperNames: ['square', 'circle', 'triangle', 'hexagon'],
     currentWallpaper: '',
