8d67298d6c3dafb9ad2bb748bc02853311ba6a98
wilvoss <wilv@allovus.com>
2023-09-28 09:50:19 -0700

    likeme 4.2.206 - bug fixes removed final score on failed blitz

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.206',
+    currentVersion: '4.2.207',
     deviceHasTouch: true,
     timeToMidnight: '24h 0m 0s',
     isGettingDailyChallenge: true,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.206';
+const CACHE_VERSION = '4.2.207';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
