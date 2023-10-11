bb2103c1e75429bc9caee6ed4a0d7187ae4a7ba1
wilvoss <wilv@allovus.com>
2023-10-10 18:19:27 -0700

    likeme 4.2.213 - UI improvements

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.211',
+    currentVersion: '4.2.213',
     deviceHasTouch: true,
     timeToMidnight: '24h 0m 0s',
     isGettingDailyChallenge: true,
