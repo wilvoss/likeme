4e22405334ab5966bd57c1b19c46adb9b66a4c91
wilvoss <wilv@allovus.com>
2024-02-10 18:35:12 -0800

    4.2.234 - bug fix

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -19,7 +19,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.233',
+    currentVersion: '4.2.234',
     deviceHasTouch: true,
     timeToMidnight: '24h 0m 0s',
     isGettingDailyChallenge: false,
