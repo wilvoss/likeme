3a4847e8bf7812ec727bdeb416f5ecb9d24e0cd9
wilvoss <wilv@allovus.com>
2024-02-12 17:59:23 -0800

    4.2.244 - ux update

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -20,7 +20,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.243',
+    currentVersion: '4.2.244',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     timeToMidnight: '24h 0m 0s',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.243';
+const CACHE_VERSION = '4.2.244';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
