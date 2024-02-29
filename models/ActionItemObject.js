36286253b9036875321fd942b9a31c7ce0dc6fe9
wilvoss <wilv@allovus.com>
2024-02-29 11:09:55 -0800

    4.2.276 - improved gem UX

--- models/ActionItemObject.js
+++ models/ActionItemObject.js
@@ -5,12 +5,14 @@ class ActionItemObject {
     this.id = spec.id === undefined ? 0 : spec.id;
     this.order = spec.order === undefined ? 0 : spec.order;
     this.icon = spec.icon === undefined ? '' : spec.icon;
-    this.name = spec.name === undefined ? 'Reward' : spec.name;
+    this.name = spec.name === undefined ? 'Reward' : spec.name + ' <icon class="info"></icon> ';
     this.description = spec.description === undefined ? '' : spec.description;
+    this.explanation = spec.explanation === undefined ? '' : spec.explanation;
     this.count = spec.count === undefined ? 0 : spec.count;
     this.currency = spec.currency === undefined ? Currencies[0] : spec.currency;
     this.maxCount = spec.maxCount === undefined ? 10 : spec.maxCount;
     this.cost = spec.cost === undefined ? 250 : spec.cost;
+    this.isExpanded = spec.isExpanded === undefined ? false : spec.isExpanded;
   }
 }
 
@@ -20,7 +22,8 @@ let ActonItems = [
     order: 0,
     icon: '🔎',
     name: 'Auto Hint',
-    description: 'Forgives a failure and highlights a mistake',
+    description: 'Highlights a mistake',
+    explanation: 'Automatically highlights a mistake on the board after you try to solve the level. Also forgives that attempt.',
     cost: 500,
     count: UseDebug ? 0 : 0,
     maxCount: 10,
@@ -30,7 +33,8 @@ let ActonItems = [
     order: 1,
     icon: '🎟️',
     name: 'Clear Level',
-    description: 'Gives one perfect level clear, no matter what',
+    description: 'Gives one perfect level',
+    explanation: 'Forgives all mistakes made on the current level, solves it perfectly, and then advances you to the next level. You must tap in-game to spend.',
     maxCount: 4,
     cost: 2500,
     count: UseDebug ? 0 : 0,
@@ -49,6 +53,7 @@ let ActonItems = [
     icon: '⌛️',
     name: 'Add 10&nbsp;sec',
     description: 'Adds 10 seconds',
+    explanation: 'With a maximum of 3 in any given game, you can add a total of 30 seconds to the clock with this powerup! You must tap in-game to spend.',
     maxCount: 3,
     cost: 5000,
     count: UseDebug ? 1 : 0,
@@ -59,6 +64,7 @@ let ActonItems = [
     icon: '⌛️',
     name: 'Add 30&nbsp;sec',
     description: 'Adds 30 seconds',
+    explanation: "It's possible to add a total of 60 seconds with this single-use powerup when combined with the 10 second one. You must tap in-game to spend. Use it wisely.",
     maxCount: 1,
     cost: 12500,
     count: UseDebug ? 0 : 0,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.275';
+const CACHE_VERSION = '4.2.276';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
