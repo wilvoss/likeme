10a03f4c17b9597d1b558f6a577a85fe6e06f28f
wilvoss <wilv@allovus.com>
2024-07-08 19:40:05 -0700

    4.2.297 - bug fixes

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -22,7 +22,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.296',
+    currentVersion: '4.2.297',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
