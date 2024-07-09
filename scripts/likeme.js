3d3d3e872079742468aad873b592eadf45cf1625
wilvoss <wilv@allovus.com>
2024-07-08 20:02:58 -0700

    4.2.299 - bug fix

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -22,7 +22,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.298',
+    currentVersion: '4.2.299',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.298';
+const CACHE_VERSION = '4.2.299';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
