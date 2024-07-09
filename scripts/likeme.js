005008af3c787e3b2418dfc37bc1234e8284ffa4
wilvoss <wilv@allovus.com>
2024-07-08 19:28:11 -0700

    4.2.296 - more UX tweaks

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -22,7 +22,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.295',
+    currentVersion: '4.2.296',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.295';
+const CACHE_VERSION = '4.2.296';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
