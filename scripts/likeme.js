74f9233f4d0bb953b17672c55b68250c4d8927b6
wilvoss <wilv@allovus.com>
2024-07-17 21:33:07 -0700

    4.2.308 - manifest ascii bug fix

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -21,7 +21,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.307',
+    currentVersion: '4.2.308',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
