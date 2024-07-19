1ab23ca662a210efedd0d760b4bbbd76ce706b0f
wilvoss <wilv@allovus.com>
2024-07-19 11:40:25 -0700

    4.2.315 - accessibility improvements - (long way to go though)

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -21,7 +21,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.314',
+    currentVersion: '4.2.315',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
