237b924b643e67c89d9c6c74f745539a488ccd07
wilvoss <wilv@allovus.com>
2025-07-04 14:06:30 -0700

    4.2.320 - new icons

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -21,7 +21,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.319',
+    currentVersion: '4.2.320',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.319';
+const CACHE_VERSION = '4.2.320';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
