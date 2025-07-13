3c6370ce711a14d9e5ad9433e6e05dfe64897afd
wilvoss <wilv@allovus.com>
2025-07-13 08:50:24 -0700

    4.2.327 - scaling allowed

--- scripts/likeme.js
+++ scripts/likeme.js
@@ -21,7 +21,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '4.2.326',
+    currentVersion: '4.2.327',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '4.2.326';
+const CACHE_VERSION = '4.2.327';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
